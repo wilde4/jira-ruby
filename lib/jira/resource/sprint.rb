3bbc085923b81d2d90e754e7ab8f0b9eae7a0ac0
Robert Wilde <wilde4@gmail.com>
2013-04-04 15:34:49 +0100

    ability to browse issues by sprint

--- lib/jira/resource/sprint.rb
+++ lib/jira/resource/sprint.rb
@@ -5,6 +5,23 @@ module JIRA
     end
 
     class Sprint < JIRA::Base
+      
+      def self.id_attribute
+        attrs[:id]
+      end
+
+      # Returns all the issues for this project
+      def issues
+        response = client.get(client.options[:rest_base_path] + "/search?jql=sprint%3D#{attrs[:id]}")
+        json = self.class.parse_json(response.body)
+        json['issues'].map do |issue|
+          client.Issue.build(issue)
+        end
+      end
+      
+
+      
+      
     end
 
   end

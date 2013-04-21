6b359f3bd6250b4e98a087560b0391cf1f2424ce
Robert Wilde <wilde4@gmail.com>
2013-04-21 16:34:49 +0100

    issue types and priorities

--- lib/jira/resource/issuetype.rb
+++ lib/jira/resource/issuetype.rb
@@ -4,7 +4,21 @@ module JIRA
     class IssuetypeFactory < JIRA::BaseFactory # :nodoc:
     end
 
-    class Issuetype < JIRA::Base ; end
+    class Issuetype < JIRA::Base
+      
+      def self.id_attribute
+        attrs[:id]
+      end
+    
+      def self.all(client)
+        response = client.get("/rest/api/2/issuetype")
+        json = parse_json(response.body)
+        json.each do |issue_type|
+          client.Issuetype.build(issue_type)
+        end
+      end
+        
+    end
 
   end
 end
--- lib/jira/resource/priority.rb
+++ lib/jira/resource/priority.rb
@@ -4,7 +4,20 @@ module JIRA
     class PriorityFactory < JIRA::BaseFactory # :nodoc:
     end
 
-    class Priority < JIRA::Base ; end
+    class Priority < JIRA::Base
+      def self.id_attribute
+        attrs[:id]
+      end
+    
+      def self.all(client)
+        response = client.get("/rest/api/2/priority")
+        json = parse_json(response.body)
+        json.each do |priority|
+          client.Priority.build(priority)
+        end
+      end
+        
+    end
 
   end
 end

a9de04b4fb715e1c5cdc774efc249ededae34a34
Robert Wilde <r.wilde@volcanic.co.uk>
2013-06-26 17:38:36 +0100

    find bugs by project

--- lib/jira/resource/project.rb
+++ lib/jira/resource/project.rb
@@ -18,7 +18,23 @@ module JIRA
       # Returns all the issues for this project
       def issues
         project_id = key rescue attrs[:key]
-        response = client.get(client.options[:rest_base_path] + "/search?jql=project%3D'#{project_id}'")
+        response = client.get(client.options[:rest_base_path] + "/search?maxResults=50&jql=project%3D'#{project_id}'")
+        json = self.class.parse_json(response.body)
+        json['issues'].map do |issue|
+          client.Issue.build(issue)
+        end
+      end
+      
+      # Returns all the issues for this project
+      def bugs
+        project_id = key rescue attrs[:key]
+        
+        # Establish which issue types are bugs
+        ids = IssueType.where(:bug => true).map{|i| "type%3D#{i.issue_type_id}"}.join("+OR+")
+        filter_by_bug = "(#{ids})%26"
+        filter_by_resolution = "(resolution!%3D'fixed'+OR+resolution!%3D'done')%26"
+        
+        response = client.get(client.options[:rest_base_path] + "/search?maxResults=300&jql=#{filter_by_bug}#{filter_by_resolution}project%3D'#{project_id}'")
         json = self.class.parse_json(response.body)
         json['issues'].map do |issue|
           client.Issue.build(issue)
